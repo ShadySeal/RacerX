af35e87c81b546110785c0beca9bc934fa6a6bb6
leevi <122457366+ShadySeal@users.noreply.github.com>
2025-08-14 15:58:30 +0300

    Made a simple movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,52 +1,46 @@
-using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float verticalInput;
-    private float mouseInputX;
-
-    private Rigidbody rb;
+    private float horizontalInput;
 
     [SerializeField] private float speed;
-    [SerializeField] private float acceleration;
-    [SerializeField] private float turnAcceleration;
-    [SerializeField] private float distanceFromGround;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float distanceFromGround = 2f;
     [SerializeField] private float angleSpeed = 15f;
+    [SerializeField] private float deceleration = 4f;
 
-    private Vector3 targetPosition;
-    private Vector3 deskUp = Vector3.zero;
+    private Vector3 deskUp;
+    private Vector3 currentVelocity;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    private void Update()
+    void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
-        mouseInputX = Input.GetAxisRaw("Mouse X");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 desiredVelocity = transform.forward * verticalInput * speed;
+
+        currentVelocity = Vector3.Lerp(currentVelocity, desiredVelocity, deceleration * Time.deltaTime);
+        Vector3 newPos = transform.position + currentVelocity * Time.deltaTime;
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, Vector3.down, out hit))
         {
-            targetPosition = new Vector3(transform.position.x, hit.point.y + distanceFromGround, transform.position.z);
-
+            newPos.y = (hit.point + Vector3.up * distanceFromGround).y;
             deskUp = hit.normal;
         }
 
-        transform.position = targetPosition;
-        transform.up = Vector3.Slerp(transform.up, deskUp, angleSpeed * Time.deltaTime);
+        transform.position = newPos;
 
-        speed = rb.linearVelocity.magnitude;
-    }
+        if (horizontalInput != 0f)
+        {
+            float turn = horizontalInput * turnSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * turn);
+        }
 
-    private void FixedUpdate()
-    {
-        rb.AddForce(rb.transform.TransformDirection(Vector3.forward) * verticalInput * acceleration, ForceMode.VelocityChange);
-        rb.AddTorque(rb.transform.up * turnAcceleration * mouseInputX, ForceMode.VelocityChange);
+        Vector3 forward = transform.forward;
+        Quaternion slopeAlignedRotation = Quaternion.LookRotation(forward, deskUp);
+        transform.rotation = Quaternion.Slerp(transform.rotation, slopeAlignedRotation, angleSpeed * Time.deltaTime);
     }
 }
