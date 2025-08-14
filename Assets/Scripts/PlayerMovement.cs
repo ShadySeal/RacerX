a13158495972bd403d69348ad257cdcd9f223a66
leevi <122457366+ShadySeal@users.noreply.github.com>
2025-08-14 12:53:23 +0300

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,6 +11,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float acceleration;
     [SerializeField] private float turnAcceleration;
+    [SerializeField] private float distanceFromGround;
+    [SerializeField] private float angleSpeed = 15f;
+
+    private Vector3 targetPosition;
+    private Vector3 deskUp = Vector3.zero;
 
     private void Start()
     {
@@ -23,6 +29,17 @@ public class PlayerMovement : MonoBehaviour
     {
         verticalInput = Input.GetAxis("Vertical");
         mouseInputX = Input.GetAxisRaw("Mouse X");
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            targetPosition = new Vector3(transform.position.x, hit.point.y + distanceFromGround, transform.position.z);
+
+            deskUp = hit.normal;
+        }
+
+        transform.position = targetPosition;
+        transform.up = Vector3.Slerp(transform.up, deskUp, angleSpeed * Time.deltaTime);
 
         speed = rb.linearVelocity.magnitude;
     }
